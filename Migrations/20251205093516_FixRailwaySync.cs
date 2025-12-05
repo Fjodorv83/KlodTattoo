694ff4510de4fc51a7c04bbfcac133b9d7ee26c4
fjodo <fjodor83>
2025-12-05 10:52:46 +0100

    FixedRail

---
+++ Migrations/20251205093516_FixRailwaySync.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KlodTattooWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRailwaySync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
